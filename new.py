8c5d03788c56057485f4f2e79eb987a6169819f0
redliner240 <itsmeullasgowda@gmail.com>
2026-03-16 12:30:23 +0530

    Create new.py

---
+++ new.py
@@ -0,0 +1,14 @@
+# Basic Python Program
+
+# Print a message
+print("Hello, World!")
+
+# Take input from the user
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+# Perform addition
+sum = a + b
+
+# Display result
+print("Sum =", sum)
